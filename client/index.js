2926ce64d4f20c2664955a1956cbfa9e23525754
Kevin Luo <kevinluo@Kevins-MacBook-Pro-2.local>
2020-03-13 19:28:55 -0400

    basic boilerplate

---
+++ client/index.js
@@ -0,0 +1,10 @@
+/**
+ * @name index.js
+ * @description Entry point into our app
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App.jsx';
+
+render (<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    devServer : {
+        publicPath : '/build/',
+    },
+    mode : 'development',
+    entry : './client/index.js',
+    output : {
+        path : path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module : {
+        rules : [
+            {
+                test : /.jsx?$/,
+                exclude: /node_modules/,
+                loader : 'babel-loader'
+            }
+        ]
+    }
+}
