0ac35af25ba6811549aeebafabd19c41f0ea50fc
Mario D'Andrea <mariodandrea14@yahoo.com>
2020-03-14 10:28:59 -0400

    Added eslint + prettier + airbnb styling

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const PORT = 3000;
 const cookieParser = require('cookie-parser');
@@ -6,4 +7,4 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.listen(PORT, () => console.log('Listening on Port: '+PORT));
+app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`));
