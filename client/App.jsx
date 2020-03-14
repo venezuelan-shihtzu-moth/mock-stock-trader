b59544db9bf27a62dc8a21ce14d44670d7279214
Kevin Luo <kevinluo@Kevins-MacBook-Pro-2.local>
2020-03-14 10:34:45 -0400

    eslint + prettier

--- client/App.jsx
+++ client/App.jsx
@@ -6,16 +6,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-    }
-    render () {
-        return (
-            <div>
-                Super Awesome Trading App!
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Super Awesome Trading App!</div>;
+  }
 }
 
 export default App;
--- client/index.js
+++ client/index.js
@@ -7,4 +7,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App.jsx';
 
-render (<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
