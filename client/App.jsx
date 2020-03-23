56f5db57c6a46bdb421ad7c64220a9bd1902f204
qkrrbtjd90 <kyu@Kyus-MacBook-Pro.local>
2020-03-23 11:52:33 -0400

    complete

--- client/App.jsx
+++ client/App.jsx
@@ -3,29 +3,29 @@
  * @description Main component for our application
  */
 
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Leaderboard from './components/Leaderboard';
 import PLchart from './components/PLchart';
 import Signup from './components/Signup';
 import About from './components/About';
 import Nav from './Nav';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-function App () {
-    return (
-        <Router>
-            <div className="App">
-                <Nav />
-                <Switch>
-                    <Route path="/about" component={About} />
-                    <Route path="leaderboard" component={Leaderboard} />
-                    <Route path="plchart" component={PLchart} />
-                    <Route path="/" exact component={Signup} />
-                </Switch>
-            </div>
-        </Router>
-    );
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="leaderboard" component={Leaderboard} />
+          <Route path="plchart" component={PLchart} />
+          <Route path="/" exact component={Signup} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
